7d0c7d476557e96976e10155d3acaa8600365956
SolidifiedRay <yc3346@nyu.edu>
2020-06-02 18:58:28 -0700

    initial commit

--- bot.py
+++ bot.py
@@ -1,19 +1,29 @@
+# -*- coding: UTF-8 -*-
 import tweepy
 import time
+import os
+from os import environ
 
-auth = tweepy.OAuthHandler('', '')
-auth.set_access_token("", '')
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-search = 'Arknights'
+search = '#アークナイツ'
 
-for tweet in tweepy.Cursor(api.search, search).items(5):
-	try:
-		print('Tweet Liked')
-		tweet.favorite()
-		time.sleep(10)
-	except StopIteration:
-		break
+while True:
+	for tweet in tweepy.Cursor(api.search, search).items(1):
+		try:
+			tweet.favorite()
+			print('Tweet Liked')
+			time.sleep(10)
+		except:
+			break
+
